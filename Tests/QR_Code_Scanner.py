1674c1531576478c9ca7cf05a5333aed79e32860
SpaceGamer <spacegamer1111@gmail.com>
2022-06-08 23:37:27 +0530

    Main file updated for QRCode scanning and detecting the nearest points.

--- Tests/QR_Code_Scanner.py
+++ Tests/QR_Code_Scanner.py
@@ -3,10 +3,23 @@ import cv2
 cap = cv2.VideoCapture(0)
 detector = cv2.QRCodeDetector()
 
-while True:
+nop = int(input("Enter number of products to be scanned: "))
+pdict = {}
+
+while (len(pdict) < nop):
     _, image = cap.read()
     res, _, _ = detector.detectAndDecode(image)
     if res:
-        print(res)
+        try:
+            lst = res.split()
+            if type(eval(lst[0])) == tuple and tuple(eval(lst[0])) not in pdict.keys():
+                pdict[tuple(eval(lst[0]))] = lst[1]
+                print("Product scanned: ", lst[0])
+        except IndexError:
+            print("Invalid QR code")
+        except Exception as e:
+            print(e)
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1)
+
+print(pdict)
--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+import cv2
+import math
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+cap = cv2.VideoCapture(0)
+detector = cv2.QRCodeDetector()
 
+nop = int(input("Enter number of products to be scanned: "))
+pdict = {}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while (len(pdict) < nop):
+    _, image = cap.read()
+    res, _, _ = detector.detectAndDecode(image)
+    if res:
+        try:
+            lst = res.split()
+            if type(eval(lst[0])) == tuple and tuple(eval(lst[0])) not in pdict.keys():
+                pdict[tuple(eval(lst[0]))] = lst[1]
+                print("Product scanned: ", lst[0])
+        except IndexError:
+            print("Invalid QR code")
+        except Exception as e:
+            print(e)
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1)
 
+cv2.destroyAllWindows()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(pdict)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def pt_order(curr_pt, pts):
+    pt_lst = []
+    print()
+    while len(pt_lst) < len(pts):
+        min_dist = float("inf")
+        for pt in pts:
+            if pt not in pt_lst:
+                dist = abs(curr_pt[0] - pt[0])*110574 + abs((curr_pt[1] - pt[1])*111320*math.cos(pt[1]*math.pi/180))
+                if dist < min_dist:
+                    min_dist = dist
+                    min_pt = pt
+        pt_lst.append(min_pt)
+        curr_pt = min_pt
+        print(min_pt, min_dist/1000, 'km')
+    return pt_lst
+
+cur_loc = (0, 0)
+
+print(pt_order(cur_loc, pdict.keys()))
+
+input("Press Enter to continue...")
